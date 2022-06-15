d59fb2c30bcf103e3a801ad4c16c1229c73e3465
Minion <nessakristo@gmail.com>
2022-06-15 23:59:13 +0100

    Just a bunch of console.logs

---
+++ script.js
@@ -0,0 +1,35 @@
+     let firstName = 'Vanessa'; //String literal
+     console.log(firstName);
+
+     let lastName = 'kris';
+     console.log(lastName)
+
+     let age = 35;  // Number literal
+     console.log(age);
+
+     const price = 5;
+     console.log(price);
+
+     let female = true; // Boolean
+
+     let mother = undefined;
+     
+     let selectedColor = null;
+
+     let person = {
+         name: 'Vanessa',
+         age: 30
+     };
+
+     console.log(person);
+
+
+     person.name = 'John';
+
+     person['name'] = 'Mary'
+
+     console.log(person.name)
+
+     let selectedColors = ['red', 'blue'];
+     selectedColors[2] = 3;
+     console.log(selectedColors);
